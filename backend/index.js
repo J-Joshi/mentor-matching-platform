3e8658ea49c713a8903ddf6032038379014e1351
J.Joshi <joshi.jj.jatin@gmail.com>
2024-12-17 03:55:21 +0530

    backend env updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ console.log("JWT_SECRET from env:", process.env.JWT_SECRET);
 const app = express();
 dotenv.config();
 // Middleware
-app.use(cors());
+app.use(cors({ origin: `${process.env.FRONTEND_URL}` }));
 app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
